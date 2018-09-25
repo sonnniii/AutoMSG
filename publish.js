e361f111d8d1ebe1661c3dfcc4a51a4265b3382f
sonnniii <sonja.goerlich@aol.de>
2018-09-25 10:33:41 +0200

    first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../automsg.zip');
+var kuduApi = 'https://automsg.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$automsg';
+var password = 'xpXiXASz9pBnWz8rwScmXqlaTbs9RM8G0nWxPu86x7FgPuQRgxgj7pYfktrz';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('automsg publish');
+  } else {
+    console.error('failed to publish automsg', err);
+  }
+});
